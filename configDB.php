0a46c74922c2454ff3c790bc15df169c09587cad
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2018-06-22 20:20:30 -0300

    Erro de conexao concertado

--- configDB.php
+++ configDB.php
@@ -1,14 +1,19 @@
 <?php
 function configDB(){
+	//Omite os erros do php
+	error_reporting(0);
+	
 	$db = Array(
 	"host" => "localhost",
 	"name" => "unfired",
 	"user" => "root",
 	"password" => "root"
 	);
-    $con = mysqli_connect($db['host'],$db['user'],$db['password'],$db['name']);
+    
+	$con = mysqli_connect($db['host'],$db['user'],$db['password'],$db['name']);
+		
 	
-	if(!$con){
+	if(mysqli_connect_errno($con)){
 		header("Location: formErro.php");
 		die();
 	}else{
